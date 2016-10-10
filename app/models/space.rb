cabbd8c36db00169360c25ac3069a8a54bdb04cc
Jonalogy <jonalogy@gmail.com>
2016-10-10 10:44:56 +0800

    Commented out test associations for likes users and spaces

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,4 @@
 class Space < ApplicationRecord
+  has_many :likes
+  has_many :users, through:likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :likes
+  has_many :spaces, through :likes
 end
+
+
+@likes = spaces.likes.all
+@likes.length
