ea784213e1ae5541afabf15f21ee43146ef3a9a5
Jonalogy <jonalogy@gmail.com>
2016-10-10 21:45:55 +0800

    Midst commit to git pull

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -24,20 +24,40 @@ class BookingsController < ApplicationController
 
   # POST /bookings
   def create
-    puts ">>>Test params[:booking]=> #{params[:booking].inspect}"
-    puts ">>>Test params[:booking][:start]=> #{params[:booking][:start].inspect}"
-    puts ">>>Test params[:booking][:end]=> #{params[:booking][:end].inspect}"
-    puts ">>>Test params[:booking][:availability_id]=> #{params[:booking][:availability_id].inspect}"
-    @avail_id = params[:booking][:availability_id]
+    # puts ">>>Test params[:booking]=> #{params[:booking].inspect}"
+    # puts ">>>Test params[:booking][:start]=> #{params[:booking][:start].inspect}"
+    # puts ">>>Test params[:booking][:end]=> #{params[:booking][:end].inspect}"
+    # puts ">>>Test params[:booking][:availability_id]=> #{params[:booking][:availability_id].inspect}"
+
+    avail_id = params[:booking][:availability_id]
+    availability = Availability.find(avail_id)
+    @avail_start = availability.start #date format
+    @avail_end = availability.end
+    @avail_seat = availability.seat
+
+    @book_start = params[:booking][:start]
+    # @book_end = params[:booking][:end]
+    # @book_seat = params[:booking][:seat]
 
     # @booking = Booking.new(booking_params)
+    # @book_start = @booking.start
+    # @book_end = @booking.end
+    # @book_seat = @booking.seat
 
-      if @booking.save
-        redirect_to @booking, notice: 'Booking was successfully created.'
-      else
-        render :new
-      end
-  end
+    puts ">>>@book_start #{@book_start.class}"
+
+    # if @book_start >= @avail_start && @book_end <= @avail_end && @book_seat <= @avail_seat
+      puts ">>> Tatatatatatata...."
+      #
+      # if @booking.save
+      #   redirect_to @booking, notice: 'Booking was successfully created.'
+      # else
+      #   render :new
+      # end
+
+    end
+
+  end #create
 
   # PATCH/PUT /bookings/1
   def update
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,6 @@
 class Space < ApplicationRecord
   # has_many :likes
   # has_many :users, through:likes
-  has_many  :shouts 
+  has_many  :shouts
+  has_many :images
 end
