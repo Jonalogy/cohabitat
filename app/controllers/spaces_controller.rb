423bb3d008e878d52feee9d94f814dfa9f81d049
celinewsm <Celine@Celines-MacBook-Pro.local>
2016-10-11 09:39:51 +0800

    fix spaces images upload sequence bug

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -66,28 +66,34 @@ class SpacesController < ApplicationController
   # PATCH/PUT /spaces/1
   def update
     image_urls = []
-    current_image_urls = Image.where(space_id: params[:id]).order('created_at ASC')
+    current_image_urls = Image.where(space_id: params[:id]).order('id ASC')
     if params[:space][:workspace_image_url_1]
+      puts "REPLACING FIRST IMAGE"
       uploaded_file_1 = params[:space][:workspace_image_url_1].path
       cloudinary_file_1 = Cloudinary::Uploader.upload(uploaded_file_1)
       image_urls << cloudinary_file_1["url"]
     else
+      puts "NOT REPLACING FIRST IMAGE"
       image_urls << current_image_urls[0].url
     end
 
     if params[:space][:workspace_image_url_2]
+      puts "REPLACING SECOND IMAGE"
       uploaded_file_2 = params[:space][:workspace_image_url_2].path
       cloudinary_file_2 = Cloudinary::Uploader.upload(uploaded_file_2)
       image_urls <<  cloudinary_file_2["url"]
     else
+      puts "NOT REPLACING SECOND IMAGE"
       image_urls << current_image_urls[1].url
     end
 
     if params[:space][:workspace_image_url_3]
+      puts "REPLACING THIRD IMAGE"
       uploaded_file_3 = params[:space][:workspace_image_url_3].path
       cloudinary_file_3 = Cloudinary::Uploader.upload(uploaded_file_3)
       image_urls << cloudinary_file_3["url"]
     else
+      puts "NOT REPLACING THIRD IMAGE"
       image_urls << current_image_urls[2].url
     end
 
@@ -97,7 +103,7 @@ class SpacesController < ApplicationController
 
       if @space.update(space_params)
 
-        @images = Image.where(space_id: params[:id])
+        @images = Image.where(space_id: params[:id]).order('id ASC')
 
         for i in (0...@images.length) do
           @images[i].update(attributes={url: image_urls[i]})
