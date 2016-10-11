9d4d84624971c96706b6074e97f341e9b1a7c5cd
Jonalogy <jonalogy@gmail.com>
2016-10-11 15:21:12 +0800

    Restructured spaces/:id view to include associated and active available listings

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,11 @@ class MainController < ApplicationController
   before_action :is_authenticated, only:[:about]
 
   def index
+    @current_user = User.find_by_id(session[:user_id])
+    puts ">>>@current_user: #{@current_user.inspect}"
   end
 
   def show
   end
-  
+
 end
